73faf70d176cd2c5cc9ab854f96b0f8549831608
S Selva Ganesh <selvaganesh.s9629@gmail.com>
2026-08-25 23:24:01 +0530

    commiting after a long time

--- converters/convertor.py
+++ converters/convertor.py
@@ -10,44 +10,14 @@ class FileConverter:
         self.input_file = input_file
         self.output_format = selected
         self.category = category
+        
+        print("input file : ", self.input_file)
+        print("output file : ", self.output_format)
+        print("category : ", self.category)
+        
 
         if self.category == "doc":
             self.document_converter()
 
     def document_converter(self):
-
-        output_file = asksaveasfilename(
-            defaultextension=f".{self.output_format}",
-            filetypes=[
-                (f"{self.output_format.upper()} files",
-                 f"*.{self.output_format}")
-            ]
-        )
-
-        if not output_file:
-            print("Cancelled")
-            return
-
-        output_dir = os.path.dirname(output_file)
-
-        subprocess.run([
-            r"C:\Program Files\LibreOffice\program\soffice.exe",
-            "--headless",
-            "--convert-to",
-            self.output_format,
-            self.input_file,
-            "--outdir",
-            output_dir
-        ])
-
-        # LibreOffice creates file automatically
-        old_output = os.path.join(
-            output_dir,
-            os.path.splitext(
-                os.path.basename(self.input_file)
-            )[0] + f".{self.output_format}"
-        )
-
-        os.rename(old_output, output_file)
-
-        print("Conversion completed")
+        pass
--- ui/test.py
+++ ui/test.py
@@ -1,19 +1,3 @@
-import numpy as np
-
-
-
-w = np.array([1, 2, -2])
-
-
-t = 3
-
-l1 = np.sum(np.abs(w))
-
-
-l2 = np.sqrt(np.sum(w**2))
-
-
-
-
-print(l1)
-print(l2)
+with open("ui\CSE202.pdf") as file:
+    print(file.read())
+    
