a9939d16b39ea7d7caae50a40343d7533b0fd232
S Selva Ganesh <selvaganesh.s9629@gmail.com>
2026-08-30 00:32:42 +0530

    Added tree datastructure implimented

---
+++ tree/tree.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+
+def find_path(graph, start, target):
+    queue = deque([[start]])
+    visited = {start}
+    
+    
+    while queue:
+        path = queue.popleft()
+        current = {start}
+        
+        
+        if current == target:
+            return path
+        
+        
+        for next_type in graph.get(current, []):
+            if next_type not in visited:
+                visited.add(next_type)
+                queue.append(path + [next_type])
+    return None
+
+
+graph = {
+    "txt": ["pdf", "html"],
+    "pdf": ["docx", "txt"],
+    "html": ["pdf"],
+    "docx": ["pdf"]
+}
+
+
+print(find_path(graph, "txt", "docx"))
