b976b0c7714ed86ade654b9462250c0fe5e25f38
S Selva Ganesh <selvaganesh.s9629@gmail.com>
2026-08-30 00:46:19 +0530

    imported the find_path function from the dir tree/tree.py

--- converters/convertor.py
+++ converters/convertor.py
@@ -1,6 +1,7 @@
 from tkinter.filedialog import asksaveasfilename
 import subprocess
 import os
+from tree.tree import find_path
 
 
 class FileConverter:
@@ -15,4 +16,10 @@ class FileConverter:
         self.current_file_type = self.input_file.split(",")[-1]
         self.output_file_type =  self.output_format.split(",")[-1]
         
-        
+        self.graph = {
+            "txt": ["pdf", "html"],
+            "pdf": ["docx", "txt"],
+            "html": ["pdf"],
+            "docx": ["pdf"]
+        }
+        print(find_path(self.graph, self.current_file_type, self.output_file_type))
